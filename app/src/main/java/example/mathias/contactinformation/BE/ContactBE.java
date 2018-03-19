c5d5b73a79432a4787f1d0b3f2870db5fed9d57c
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2018-03-19 16:47:27 +0100

    Got Recycler view working

--- app/src/main/java/example/mathias/contactinformation/BE/ContactBE.java
+++ app/src/main/java/example/mathias/contactinformation/BE/ContactBE.java
@@ -1,9 +1,35 @@
 package example.mathias.contactinformation.BE;
 
+import android.location.Location;
+import android.media.Image;
+
+import java.util.Date;
+
 /**
  * Created by Mathias on 19/03/2018.
  */
 
 public class ContactBE {
-    //To be implemented
+
+    public String mName;
+    public String mAddress;
+    public Location mLocation;
+    public String mPhoneNumber;
+    public String mMailAddress;
+    public String mWebsite;
+    public Date mBirthDay;
+    public Image mPicture;
+
+    public ContactBE(String name) {
+        mName = name;
+    }
+
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
 }
