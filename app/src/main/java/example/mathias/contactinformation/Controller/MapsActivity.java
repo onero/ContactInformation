d1632a627f95e8cc71d6a7b36937691cb9a4f9d3
Sk0vgaard <Sk0vgaard@live.dk>
2018-03-20 09:11:02 +0100

    testing

--- app/src/main/java/example/mathias/contactinformation/Controller/MapsActivity.java
+++ app/src/main/java/example/mathias/contactinformation/Controller/MapsActivity.java
@@ -42,7 +42,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in SYDNEY"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
