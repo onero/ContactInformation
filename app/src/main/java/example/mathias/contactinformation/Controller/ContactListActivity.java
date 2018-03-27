7b35344500b57827c07b524f3570b27578256b91
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2018-03-27 10:25:44 +0200

    Added documentation on ContactListActivity

--- app/src/main/java/example/mathias/contactinformation/Controller/ContactListActivity.java
+++ app/src/main/java/example/mathias/contactinformation/Controller/ContactListActivity.java
@@ -3,7 +3,6 @@ package example.mathias.contactinformation.Controller;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,40 +13,49 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import example.mathias.contactinformation.BE.ContactBE;
-import example.mathias.contactinformation.Database.ContactBaseHelper;
 import example.mathias.contactinformation.Model.ContactModel;
 import example.mathias.contactinformation.R;
 
 public class ContactListActivity extends AppCompatActivity {
 
+    // 
     public static Dialog myDialog;
+
+    // Stored Context.
     private Context mContext;
 
+    // Stored Contact RecyclerViewAdapter.
     private ContactRecyclerViewAdapter adapter;
+
+    // Stored RecyclerView.
     private RecyclerView recyclerView;
 
+    /**
+     * Instanciation of Context, Dialog, RecyclerView and RecyclerViewAdapter.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         mContext = this;
 
         super.onCreate(savedInstanceState);
         myDialog = new Dialog(mContext);
-        /**
-         * Recyclerview
-         */
+
+        // Recyclerview
         setContentView(R.layout.activity_main_recycler);
         recyclerView = findViewById(R.id.recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
 
-        /**
-         * Incredible recyclerview adapter
-         */
+        // Incredible recyclerview adapter
         adapter = new ContactRecyclerViewAdapter(ContactModel.get(mContext));
-
         recyclerView.setAdapter(adapter);
     }
 
-    // When starting up the application create the menu.
+    /**
+     * When starting up the application create the menu.
+     * @param menu
+     * @return
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -55,7 +63,11 @@ public class ContactListActivity extends AppCompatActivity {
         return true;
     }
 
-    // Switch for our menu so it is easy to add more later on.
+    /**
+     * Switch for our menu so it is easy to add more later on.
+     * @param item
+     * @return
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle item selection
@@ -74,7 +86,9 @@ public class ContactListActivity extends AppCompatActivity {
         }
     }
 
-    // Adding mock contact.
+    /**
+     * Add a Contact.
+     */
     private void addContact() {
 
         ContactBE contactToAdd = new ContactBE();
@@ -88,7 +102,9 @@ public class ContactListActivity extends AppCompatActivity {
         recyclerView.scrollToPosition(ContactModel.get(mContext).getContacts().size() - 1);
     }
 
-    // Starts up a new Activity for maps.
+    /**
+     * Starts up a new Activity for maps.
+     */
     private void map() {
         Intent intent = new Intent(ContactListActivity.this, MapsActivity.class);
         startActivity(intent);
